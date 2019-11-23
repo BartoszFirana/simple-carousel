7ba29ebdcf00e5dbe1bb526b59b65aa9c56458c9
BartoszFirana <49678089+BartoszFirana@users.noreply.github.com>
2019-11-23 16:39:20 +0100

    Handler of slider added
    - input switcher is working,
    - slider is working in simple version ( the handler of slider needs to be increased by exposing a central image )

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,48 @@
-console.log('Elo');
+//switcher
+const sliderBtns = document.querySelector('.buttons');
+const switchButton = document.getElementById('switch');
+switchButton.addEventListener('click', () => {
+    if (switchButton.checked != true) {
+        sliderBtns.style.display = "none";
+    } else {
+        sliderBtns.style.display = "block";
+    }
+});
+
+//CounterSlide
+const carouselImages = document.querySelectorAll('.carousel__image');
+const slide = document.querySelector('.carousel');
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+
+//buttons
+const btnNext = document.querySelector('.carousel__button--next');
+const btnPrev = document.querySelector('.carousel__button--prev');
+
+btnPrev.addEventListener('click', () => {
+    counter--
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+btnNext.addEventListener('click', () => {
+    counter++
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slide.addEventListener('transitionend', () => {
+    if (counter === carouselImages.length - 3) {
+        slide.style.transition = "none";
+        counter = 1;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (counter < 1) {
+        slide.style.transition = "none";
+        counter = carouselImages.length - 4;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+})
