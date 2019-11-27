b7480f785421bf1c7239cc9fd9eec74e2896f166
BartoszFirana <49678089+BartoszFirana@users.noreply.github.com>
2019-11-27 22:44:13 +0100

    Basis for image selection handler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,15 +20,21 @@ let data = [
     {
         src: image5,
     }
-]
+];
+
 const galleryContainer = document.querySelector('.carousel');
+const checkbox = document.querySelector('.switch__container--input');
+
+const btns = document.querySelector('.carousel__button');
 const btnPrev = document.querySelector('.carousel__button--prev');
 const btnNext = document.querySelector('.carousel__button--next');
+
+const lastPicture = data.length - 1;
 
 setContent();
 
 btnPrev.addEventListener('click', () => {
-    data.splice(0, 0, data[data.length - 1]);
+    data.splice(0, 0, data[lastPicture]);
     data.splice(data.length - 1, 1);
     setContent();
 });
@@ -41,21 +47,29 @@ btnNext.addEventListener('click', () => {
 
 window.addEventListener('resize', setContent);
 
+checkbox.addEventListener('click', () => {
+    if (!checkbox.checked) {
+        btns.classList.add('carousel__button--disable');
+    } else {
+        btns.classList.remove('carousel__button--disable');
+    }
+});
+
 function setContent() {
 
-    if (window.innerWidth < 700) {
+    if (window.innerWidth < 700 || data.length <= 3) {
         galleryContainer.innerHTML = `
-    <img src="${data[0].src}" alt="" class="carousel__image">
-    <img src="${data[1].src}" alt="" class="carousel__image active">
-    <img src="${data[2].src}" alt="" class="carousel__image">
+            <button class="image2"><img src="${data[0].src}" alt="" class="carousel__image"></button>
+            <img src="${data[1].src}" alt="" class="carousel__image active">
+            <button class="image4"><img src="${data[2].src}" alt="" class="carousel__image"></button>
 `;
     } else {
         galleryContainer.innerHTML = `
-        <img src="${data[4].src}" alt="" class="carousel__image">
-        <img src="${data[0].src}" alt="" class="carousel__image">
-        <img src="${data[1].src}" alt="" class="carousel__image active">
-        <img src="${data[2].src}" alt="" class="carousel__image">
-        <img src="${data[3].src}" alt="" class="carousel__image">
+            <button class="image1"><img src="${data[lastPicture].src}" alt="" class="carousel__image"></button>
+            <button class="image2"><img src="${data[0].src}" alt="" class="carousel__image"></button>
+            <img src="${data[1].src}" alt="" class="carousel__image active">
+            <button class="image4"><img src="${data[2].src}" alt="" class="carousel__image"></button>
+            <button class="image5"><img src="${data[3].src}" alt="" class="carousel__image"></button>
     `;
     }
-}
+};
