c7ff273ab44e262ca3f702a130f85c2adb80a1a9
BartoszFirana <49678089+BartoszFirana@users.noreply.github.com>
2019-11-28 15:15:31 +0100

    Recent corrections to the main task list
    - added handler for rotating the carousel by two pictures,
    - referred to the code review: class names was changed, index.js is more clean and intuitive now,

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,55 +4,15 @@ import image3 from '../../assets/3.png';
 import image4 from '../../assets/4.png';
 import image5 from '../../assets/5.png';
 
-let data = [
-    {
-        src: image1,
-    },
-    {
-        src: image2,
-    },
-    {
-        src: image3,
-    },
-    {
-        src: image4,
-    },
-    {
-        src: image5,
-    }
-];
+const images = [image1, image2, image3, image4, image5];
 
 const galleryContainer = document.querySelector('.carousel');
-const checkbox = document.querySelector('.switch__container--input');
-
+const checkbox = document.querySelector('.switch__input');
 const btns = document.querySelector('.carousel__button');
-const btnPrev = document.querySelector('.carousel__button--prev');
-const btnNext = document.querySelector('.carousel__button--next');
 
-const lastPicture = data.length - 1;
+const lastPicture = images.length - 1;
 
 setContent();
-
-const image44 = document.querySelector('.image4'); // testowe działanie
-
-btnPrev.addEventListener('click', () => {
-    data.splice(0, 0, data[lastPicture]);
-    data.splice(data.length - 1, 1);
-    setContent();
-});
-
-btnNext.addEventListener('click', () => {
-    data.splice(data.length, 0, data[0]);
-    data.splice(0, 1);
-    setContent();
-});
-
-image44.addEventListener('click', () => {
-    data.splice(data.length, 0, data[0]);
-    data.splice(0, 1);
-    setContent();
-    console.log("click");
-}); // testowe działanie
 
 window.addEventListener('resize', setContent);
 
@@ -64,21 +24,54 @@ checkbox.addEventListener('click', () => {
     }
 });
 
-function setContent() {
-
-    if (window.innerWidth < 700 || data.length <= 3) {
-        galleryContainer.innerHTML = `
-            <button class="image2"><img src="${data[0].src}" alt="" class="carousel__image"></button>
-            <img src="${data[1].src}" alt="" class="carousel__image active">
-            <button class="image4"><img src="${data[2].src}" alt="" class="carousel__image"></button>
-`;
-    } else {
-        galleryContainer.innerHTML = `
-            <button class="image1"><img src="${data[lastPicture].src}" alt="" class="carousel__image"></button>
-            <button class="image2"><img src="${data[0].src}" alt="" class="carousel__image"></button>
-            <img src="${data[1].src}" alt="" class="carousel__image active">
-            <button class="image4"><img src="${data[2].src}" alt="" class="carousel__image"></button>
-            <button class="image5"><img src="${data[3].src}" alt="" class="carousel__image"></button>
-    `;
+btns.addEventListener('click', (event) => {
+    if (event.target.classList.value === "carousel__button--prev") {
+        carouselTurnLeft()
+    } if (event.target.classList.value === "carousel__button--next") {
+        carouselTurnRight()
     }
+})
+
+galleryContainer.addEventListener("click", (event) => {
+    if (event.target.id === "1") {
+        carouselTurnLeft()
+    } if (event.target.id === "3") {
+        carouselTurnRight()
+    } if (event.target.id === "0") {
+        carouselTurnTwiceLeft()
+    } if (event.target.id === "4") {
+        carouselTurnTwiceRight()
+    }
+});
+
+function carouselTurnLeft() {
+    images.splice(0, 0, images[lastPicture]);
+    images.splice(images.length - 1, 1);
+    setContent();
+}
+
+function carouselTurnRight() {
+    images.splice(images.length, 0, images[0]);
+    images.splice(0, 1);
+    setContent();
+}
+
+function carouselTurnTwiceLeft() {
+    images.splice(0, 0, images[lastPicture]);
+    images.splice(0, 0, images[lastPicture]);
+    images.splice(images.length - 2, 2);
+    setContent();
+}
+
+function carouselTurnTwiceRight() {
+    images.splice(images.length, 0, images[0]);
+    images.splice(images.length, 0, images[1]);
+    images.splice(0, 2);
+    setContent();
+}
+
+function setContent() {
+    galleryContainer.innerHTML = images.map((image, index) => (`
+            <img src="${image}" alt="" id=${index} class="${index === 2 ? "carousel__image active" : "carousel__image"}">
+            `)).join(``)
 };
